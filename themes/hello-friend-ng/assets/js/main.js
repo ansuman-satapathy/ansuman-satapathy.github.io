0f76b0168b72ea8dbf7e1d566d575d919bd059a3
ansuman-satapathy <satapathyansuman12@gmail.com>
2026-03-16 20:05:00 +0530

    Fix deployment: merge theme submodule and add custom UI features

---
+++ themes/hello-friend-ng/assets/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Theming.
+ *
+ * Supports the preferred color scheme of the operation system as well as
+ * the theme choice of the user.
+ *
+ */
+const themeToggle = document.querySelector(".theme-toggle");
+
+// Minimal script if any other functionality is needed later.
+// Currently theme switching is disabled.
